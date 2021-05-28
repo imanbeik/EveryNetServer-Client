9660494d00a0b00b3221515b4a4ded309a0b68df
amir14a <a.abbasj@yahoo.com>
2021-05-28 20:50:51 +0430

    check message type add params for post method fix path:port bug

--- ClientApp.py
+++ ClientApp.py
@@ -7,32 +7,39 @@ ACCESS_TOKEN = None
 PORT = None
 URI = 'ws://everynetserver.ga:8924'
 
+
 async def forwarder():
     global PORT, URI
     async with websockets.connect(URI, extra_headers=[('access_token', ACCESS_TOKEN)]) as websocket:
         while True:
-            full_request_json = await websocket.recv()
-            full_request = json.loads(full_request_json)
-            
-            if full_request["method"] == "GET":
-                ans = requests.get(f"http://localhost{full_request['path']}:{PORT}", headers=full_request['headers'])
-            else:
-                ans = requests.post(f"http://localhost{full_request['path']}:{PORT}", headers=full_request['headers'])
-            
-            response = {}
-            response['text'] = ans.text
-            response['headers'] = ans.headers
-            response_json = json.dumps(response)
-            
-            await websocket.send(response_json)
+            message = json.loads(await websocket.recv())
+            if message["type"] == "request":
+                full_request = message["data"]
+
+                if full_request["method"] == "GET":
+                    ans = requests.get(f"http://localhost:{PORT}{full_request['path']}",
+                                       headers=full_request['headers'])
+                else:
+                    ans = requests.post(f"http://localhost:{PORT}{full_request['path']}",
+                                        headers=full_request['headers'],
+                                        data=full_request['params'])
+
+                response = {'text': ans.text, 'headers': ans.headers, 'code': ans.status_code}
+                response_json = json.dumps(response)
+
+                await websocket.send(response_json)
+
+            elif message["type"] == "message":
+                print(message["data"])
+
 
 if __name__ == "__main__":
     PORT = input("Enter port you wanna requests forward to it: ")
     ACCESS_TOKEN = input("Enter your access token: ")
-    
+
     try:
         asyncio.get_event_loop().run_until_complete(forwarder())
     except KeyboardInterrupt:
         pass
-    
+
     print("Server stopped")
